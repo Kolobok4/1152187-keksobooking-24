87cf96e72338b7f68aa75badc1abdf77fc41b652
Andrii Bilmak <bilmak14@gmail.com>
2021-09-28 00:07:07 +0300

    2.15. Нужно больше функций
    Вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let getRandomInt = (min, max) => {
+
+  if (min >= max || min < 0) {
+    throw new RangeError('Недопустимый диапазон чисел')
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/*Получение случайного целого числа в заданном интервале, включительно
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random*/
+
+let getRandomFloat = (min, max, floatPoint) => {
+
+  if (min >= max || min < 0) {
+    throw new Error('Некорректный диапазон чисел');
+  }
+  return parseFloat((Math.random() * (max - min) + min).toFixed(floatPoint));
+
+};
+
+/*Метод форматирует число, используя запись с фиксированной запятой
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+Функция принимает строку в качестве аргумента и возвращает десятичное число
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseFloat*/
+
