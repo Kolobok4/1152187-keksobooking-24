8ee600062badaa9bd156889427c769e9426238ba
Andrii Bilmak <bilmak14@gmail.com>
2021-09-30 01:00:39 +0300

    fix function

--- js/main.js
+++ js/main.js
@@ -1,28 +1,34 @@
 const getRandomInt = (min, max) => {
 
-  if (min >= max || min < 0) {
-    throw new RangeError('Недопустимый диапазон чисел');
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+  if (min > max) {
+    [min, max] = [max, min]
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 
 };
 
-getRandomInt();
+getRandomInt(1, 5);
 /*Получение случайного целого числа в заданном интервале, включительно
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random*/
 
 const getRandomFloat = (min, max, floatPoint) => {
 
-  if (min >= max || min < 0) {
-    throw new Error('Некорректный диапазон чисел');
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+  if (min > max) {
+    [min, max] = [max, min]
   }
 
   return parseFloat((Math.random() * (max - min) + min).toFixed(floatPoint));
 
 };
 
-getRandomFloat();
+getRandomFloat(1.2, 2.3, 3);
 /*Метод форматирует число, используя запись с фиксированной запятой
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
 Функция принимает строку в качестве аргумента и возвращает десятичное число
