f91a924a8d9c75b14aa4c256bc7c2bf5ddafddba
Andrii Bilmak <bilmak14@gmail.com>
2021-10-06 02:21:50 +0300

    module4-task1 done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const getRandomInt = (min, max) => {
-
   if (min < 0 || max < 0) {
     return -1;
   }
@@ -16,7 +15,6 @@ const getRandomInt = (min, max) => {
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random*/
 
 const getRandomFloat = (min, max, floatPoint) => {
-
   if (min < 0 || max < 0) {
     return -1;
   }
